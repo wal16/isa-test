2052d63a9c1d3f647d59e8fc09d7a5148cc3edc7
Waldemar Mozoła <waldemar.mozola@onet.pl>
2017-12-10 16:02:03 +0100

    change patient

--- PatientService.java
+++ PatientService.java
@@ -5,9 +5,13 @@ class PatientService {
     private final List<Patient> patients = new ArrayList<>();
 
     PatientService() {
-        this.patients.add(new Patient("Jan", "Kowalski", 11));
+
         this.patients.add(new Patient("Adam", "Nowak", 12));
         this.patients.add(new Patient("Maria", "Nowakowska", 13));
+        this.patients.add(new Patient("Tomek", "Nowak", 13));
+        this.patients.add(new Patient("Daria", "Kowska", 13));
+        this.patients.add(new Patient("Mateusz", "NLis", 13));
+        this.patients.add(new Patient("Bartek", "LU", 13));
     }
 
     List<Patient> getAll() {
