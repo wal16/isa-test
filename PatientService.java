070aba96f50e9cbf91ee7526f7e6e2fde8d257fe
Waldemar Mozoła <waldemar.mozola@onet.pl>
2017-12-10 16:15:05 +0100

    new deal

--- PatientService.java
+++ PatientService.java
@@ -6,12 +6,16 @@ class PatientService {
 
     PatientService() {
 
-        this.patients.add(new Patient("Adam", "Nowak", 12));
+
         this.patients.add(new Patient("Maria", "Nowakowska", 13));
         this.patients.add(new Patient("Tomek", "Nowak", 13));
         this.patients.add(new Patient("Daria", "Kowska", 13));
         this.patients.add(new Patient("Mateusz", "NLis", 13));
-        this.patients.add(new Patient("Bartek", "LU", 13));
+        this.patients.add(new Patient("Marek", "Inglot", 13));
+        this.patients.add(new Patient("Lech", "Gacg", 13));
+        this.patients.add(new Patient("Ziemowit", "Podolski", 13));
+        this.patients.add(new Patient("Zbigniw", "Koalski", 13));
+        this.patients.add(new Patient("Igor", "Ul", 13));
     }
 
     List<Patient> getAll() {
